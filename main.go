2f09105c70af17e90597db768f1e0dc86ba8251e
ashkan-developer <ashkanbaqayeri@gmail.com>
2023-02-11 20:32:10 +0330

    update the main file and routes

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"github.com/ashkan-developer/ecommerce-golang/controllers"
+	"github.com/ashkan-developer/ecommerce-golang/databasea"
+	"github.com/ashkan-developer/ecommerce-golang/middleware"
+	"github.com/ashkan-developer/ecommerce-golang/routes"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+)
 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(databasea.ProductData(databasea.Client, "Products"), databasea.UserData(databasea.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("addtocart", app.AddToCart())
+	router.GET("removeitem", app.RemoveItem())
+	router.GET("cartcheckout", app.BuyFromCart())
+	router.GET("instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,14 @@
 package routes
 
 import (
+	"github.com/ashkan-developer/ecommerce-golang/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("/user/signup")
-	incomingRoutes.POST("/user/login")
-	incomingRoutes.POST("/admin/addproduct")
-	incomingRoutes.GET("/users/productview")
-	incomingRoutes.GET("/users/search")
+	incomingRoutes.POST("/user/signup", controllers.SignUp())
+	incomingRoutes.POST("/user/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
 }
